3fcd8bc15090e4f201baa37f93742a7093de7666
Felix Khoja <o.khoja93@gmail.com>
2021-03-30 22:58:22 -0400

    Creates the FeedItem Model.

---
+++ Feed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  Feed
+//
+//  Created by Omran Khoja on 3/30/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
