b54a82b9e65c72792987c2aca15d96c54d976670
Felix Khoja <o.khoja93@gmail.com>
2021-03-31 03:02:24 -0400

    Passes tests: For additional non 200 HTTP status codes.

--- FeedTests/RemoteFeedLoaderTests.swift
+++ FeedTests/RemoteFeedLoaderTests.swift
@@ -44,12 +44,13 @@ class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversErrorOnNon200HTTPResponse() {
         let (sut, client) = makeSUT()
         
-        var capturedErrors = [RemoteFeedLoader.Error]()
-        sut.load { capturedErrors.append($0) }
-        
-        let clientError = NSError(domain: "Test", code: 0)
-        client.complete(withStatusCode: 400)
-        XCTAssertEqual(capturedErrors, [.invalidData])
+        let sampleResponseCodes = [199, 201, 300, 400, 500]
+        sampleResponseCodes.enumerated().forEach { index, code in
+            var capturedErrors = [RemoteFeedLoader.Error]()
+            sut.load { capturedErrors.append($0) }
+            client.complete(withStatusCode: code, at: index)
+            XCTAssertEqual(capturedErrors, [.invalidData])
+        }
     }
     
     
