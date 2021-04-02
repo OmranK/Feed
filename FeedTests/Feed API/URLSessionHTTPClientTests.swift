91b20f2240455ebcb8a73deb1fbb5ca84c6d71c8
Felix Khoja <o.khoja93@gmail.com>
2021-04-02 01:53:03 -0400

    Removes unnecessary test.

--- FeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ FeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -10,15 +10,6 @@ import Feed
 
 class URLSessionHTTPClientTests: XCTestCase {
     
-    func test_getFromURL_createsDataTaskWithURL() {
-        let url = URL(string: "http://any-url.com")!
-        let session = URLSessionSpy()
-        let sut = URLSessionHTTPClient(session: session)
-        sut.get(from: url)
-        
-        XCTAssertEqual(session.receivedURLs, [url])
-    }
-    
     func test_getFromURL_resumesDataTaskWithURL() {
         let url = URL(string: "http://any-url.com")!
         let session = URLSessionSpy()
@@ -39,14 +30,11 @@ class URLSessionHTTPClientTests: XCTestCase {
         
         private var stubs = [URL: URLSessionDataTask]()
         
-        var receivedURLs = [URL]()
-        
         func stub(url: URL, task: URLSessionDataTask) {
             stubs[url] = task
         }
         
         override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
-            receivedURLs.append(url)
             return stubs[url] ?? FakeURLSessionDataTask()
         }
     }
